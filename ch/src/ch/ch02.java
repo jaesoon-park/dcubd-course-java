1e3843670b176dd35a5d394e3018f8be190edc89
jaesoon-park <wotns2606@cu.ac.kr>
2019-04-05 09:56:54 +0900

    ch02!

---
+++ ch/src/ch/ch02.java
@@ -0,0 +1,25 @@
+package ch;
+
+public class ch02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		char c1 = 'A';		
+		char c2 = 65;
+		char c3 = '\u0041';
+		
+		char c4 = 44032;
+		char c6 = '\uac00';
+		
+		int unicode = c1;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c6);
+		System.out.println(unicode);
+	}
+
+}
