83d05debe9d7bf4462e90d5c7de91f0ddfaf325e
jaesoon-park <wotns2606@cu.ac.kr>
2019-04-15 15:55:28 +0900

    ch06_217

---
+++ ch/src/ch/ch06_217ex.java
@@ -0,0 +1,19 @@
+package ch;
+
+public class ch06_217ex {
+
+	public static void main(String[] args) {
+		ch06_217 myCalc = new ch06_217();
+		myCalc.powerOn();
+
+		int result1 = myCalc.plus(5, 6);
+		System.out.println("result1: " + result1);
+		
+		byte x =10;
+		byte y = 4;
+		double result2 = myCalc.divide(x, y);
+		System.out.println("result2: " + result2);
+		myCalc.powerOff();
+	}
+
+}
