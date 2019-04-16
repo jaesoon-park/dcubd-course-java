244f874ae1decd0176814fbe51c981b9296d5638
jaesoon-park <wotns2606@cu.ac.kr>
2019-04-16 15:22:15 +0900

    ch06_238~239

---
+++ ch/src/ch/ch06_239.java
@@ -0,0 +1,15 @@
+package ch;
+
+public class ch06_239 {
+
+	public static void main(String[] args) {
+		double result1 = 10 * 10 * ch06_238.pi;
+		int result2 = ch06_238.plus(10,5);
+		int result3 = ch06_238.minus(10,5);
+		
+		System.out.println("result1 : " + result1);
+		System.out.println("result2 : " + result2);
+		System.out.println("result3 : " + result3);
+	}
+
+}
