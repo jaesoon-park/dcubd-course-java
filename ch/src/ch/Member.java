e8b5ade66a58280576da53276934cb09b48ad0e9
jaesoon-park <wotns2606@cu.ac.kr>
2019-04-30 15:46:12 +0900

    ch06_ex13,14,15(279,280)

---
+++ ch/src/ch/Member.java
@@ -0,0 +1,18 @@
+package ch;
+
+public class Member {
+
+	String name;
+	String id;
+	String password;
+	int age;
+	
+	Member(String name,String id){
+		this.name = name;
+		this.id = id;
+		
+	}
+
+}
+
+
