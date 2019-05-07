48387c0ddcbf436ac0c9b1fe2651f7c133b01db8
jaesoon-park <wotns2606@cu.ac.kr>
2019-05-07 16:01:39 +0900

    ch07_296~297

---
+++ ch/src/ch/Computer.java
@@ -0,0 +1,10 @@
+package ch;
+
+public class Computer extends Calculator{
+	@Override
+	double areaCircle(double r) {
+		System.out.println("Computer °´Ã¼ÀÇ  areaCircle() ½ÇÇà");
+		return Math.PI *r * r;
+	}
+
+}
