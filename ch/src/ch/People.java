a997b62a42ac95f27dae82c5f7e11125dff9a33f
jaesoon-park <wotns2606@cu.ac.kr>
2019-05-07 15:33:49 +0900

    ch07_294~295

---
+++ ch/src/ch/People.java
@@ -0,0 +1,13 @@
+package ch;
+
+public class People {
+
+	public String name;
+	public String ssn;
+	
+	public People(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+	}
+
+}
---
+++ ch/src/ch/StudentExample.java
@@ -0,0 +1,14 @@
+package ch;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		Student student =new Student("ÄO±æµ¿","12345~1234567",1);
+		System.out.println("name : " + student.name);
+		System.out.println("ssn : " + student.ssn);
+		System.out.println("student : " + 
+								student.studentNo);
+
+	}
+
+}
