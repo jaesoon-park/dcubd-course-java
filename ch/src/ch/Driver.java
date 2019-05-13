d4f0196dfa016576f1c246e83ed00ecac7ac8c23
jaesoon-park <wotns2606@cu.ac.kr>
2019-05-13 15:38:17 +0900

    ch07_322~324

---
+++ ch/src/ch/Driver.java
@@ -0,0 +1,7 @@
+package ch;
+
+public class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+}
---
+++ ch/src/ch/DriverExample.java
@@ -0,0 +1,16 @@
+package ch;
+
+public class DriverExample {
+
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+		
+		Bus bus = new Bus();
+		Taxi taxi = new Taxi();
+		
+		driver.drive(bus);
+		driver.drive(taxi);
+
+	}
+
+}
