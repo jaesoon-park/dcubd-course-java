e335107407ca4a0cfcb024483c08c09528ea74f4
jaesoon-park <wotns2606@cu.ac.kr>
2019-05-21 16:45:57 +0900

    interface

--- ch/src/ch08/RemoteControlExample.java
+++ ch/src/ch08/RemoteControlExample.java
@@ -10,7 +10,35 @@ public class RemoteControlExample {
 		rc = new Audio();
 		rc.turnOn();
 		rc.setMute(true);
+		
+		rc = new RemoteControl() {
+			private int volume;
+			
+			public void turnOn() {
+				System.out.println("스마트폰를 켭니다.");
+			}
+			//turnOff()추상 메소드의 실체 메소드
+			public void turnOff() {
+				System.out.println("스마트폰를 끕니다.");
+			}
+			//setVolume()추상 메소드의 실체 메소드
 
+			public void setVolume(int volume) {
+				if(volume>RemoteControl.MAX_VOLUME) {
+					this.volume = RemoteControl.MAX_VOLUME;
+				}
+				else if(volume>RemoteControl.MIX_VOLUME) {
+					this.volume = RemoteControl.MIX_VOLUME;
+				}else {
+					this.volume = volume;
+				}
+				System.out.println("현재 스마트폰 볼륨: "+ this.volume);
+			}
+		};
+			rc.turnOn();
+			rc.turnOff();
+			rc.setMute(true);
+		}
 	}
 
-}
+
