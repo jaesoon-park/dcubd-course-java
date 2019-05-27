bf3cdc033bec676460b1c074ea08cf903308925d
jaesoon-park <wotns2606@cu.ac.kr>
2019-05-27 16:09:10 +0900

    ch08_386

---
+++ ch/src/ch08/Cat.java
@@ -0,0 +1,8 @@
+package ch08;
+
+public class Cat implements Soundable{
+	@Override 
+	public String sound() {
+		return "¾ß¿Ë";
+	}
+}
---
+++ ch/src/ch08/Dog.java
@@ -0,0 +1,8 @@
+package ch08;
+
+public class Dog implements Soundable{
+	@Override 
+	public String sound() {
+		return "¸Û¸Û";
+	}
+}
